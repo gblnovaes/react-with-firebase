192f0baed966d84d4d93537e904f2f6259a0c370
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-09-27 10:09:07 -0300

    added flow list and edit using firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import firebase from './firebase.config'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
-import './index.css'
+import './app.css'
 function App() {
   
+  const [idPost,setIdPost] = useState('')
   const [titulo, setTitulo] = useState('')
   const [autor,  setAutor] = useState('')
+  const [posts, setPosts] = useState([])
   
   async function handleAdd(){
+    console.log("clicou")
       await firebase.firestore().collection('posts')
       .add({
         titulo: titulo,
@@ -21,15 +24,66 @@ function App() {
       })
   }
   
+  async function editPost(){
+    await firebase.firestore().collection('posts').doc(idPost)
+    .update({
+      titulo,
+      autor
+    })
+    
+  }
+  
+ 
+  
+  useEffect(() => {
+    async function loadPosts() {
+      await firebase.firestore().collection('posts').onSnapshot((doc) => {
+      let lista = []
+        doc.forEach((item) =>{
+          lista.push({
+            id: item.id,
+            titulo: item.data().titulo,
+            autor: item.data().autor
+          })
+        })
+        setPosts(lista)
+      })
+     
+    }
+    loadPosts()
+    
+  }, [posts]);
+  
   return (
     <div className='container'>
      <div className="form-container">
-      <label htmlFor="">Titulo</label>
-        <textarea cols="10" rows="10" value={titulo} onChange= {(e) => setTitulo(e.target.value)}></textarea>
+     <label htmlFor="">ID</label>
+     <input type="text" placeholder="Digite o Id" value={idPost} onChange={(e) => setIdPost(e.target.value)}/>
+     
+      <label htmlFor="">Titulo1</label>
+        <textarea cols="10" rows="3" value={titulo} onChange= {(e) => setTitulo(e.target.value)}></textarea>
         <label htmlFor="">Autor</label>
         <input type="text" placeholder='Digite o nome do Autor' value={autor} onChange={ (e) => setAutor(e.target.value) } />
         <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={editPost}>Editar</button>
      </div>
+     
+     <div className="list-container">
+        <ul>
+        {
+          posts.map((post) =>{
+            return(
+                <li key={post.id}>
+                  <span>  {post.id } -- </span> <br />
+                  <span>  {post.titulo } -- </span> <br />
+                  <span>  {post.autor }</span> <br />
+                </li>
+            )
+          })
+        }
+        </ul>
+     </div>
+     
     </div>
   );
 }
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-
+import env from 'react-dotenv'
+ 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey:   env.API_KEY,
+    authDomain:   env.AUTH_DOMAIN,
+    projectId:   env.PROJECT_ID,
+    storageBucket:   env.STORAGE_BUCKET,
+    messagingSenderId:   env.MESSAGING_SENDER_ID,
+    appId:   env.APP_ID,
+    measurementId:   env.MEASUREMENT_ID
   };
   
 if(!firebase.apps.length){
