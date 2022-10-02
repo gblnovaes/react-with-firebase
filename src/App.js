a974c23c72f58225e60e4cfaff0ec4e2e56388d1
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-10-01 23:36:25 -0300

    adcionado funcionalidade de adicionar usuario no firebase

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
   const [autor,  setAutor] = useState('')
   const [posts, setPosts] = useState([])
   
+  const [email,setEmail] = useState('')
+  const [senha,setSenha] = useState('')
+  
   async function handleAdd(){
     console.log("clicou")
       await firebase.firestore().collection('posts')
@@ -70,38 +73,63 @@ function App() {
     
   }, [posts]);
   
+  async function novoUsuario(){
+    await firebase.auth().createUserWithEmailAndPassword(email,senha)
+    .then(()=>{
+      console.log("Cadastrado com sucesso")
+      setEmail('')
+      setSenha('')
+    })
+    .catch((error)=>{
+      console.log("Error :" + error)
+    })
+  }
+  
   return (
+    
+    <>
+    
     <div className='container'>
-     <div className="form-container">
-     <label htmlFor="">ID</label>
-     <input type="text" placeholder="Digite o Id" value={idPost} onChange={(e) => setIdPost(e.target.value)}/>
-     
-      <label htmlFor="">Titulo1</label>
-        <textarea placeholder="Digite o titulo" cols="10" rows="3" value={titulo} onChange= {(e) => setTitulo(e.target.value)}></textarea>
-        <label htmlFor="">Autor</label>
-        <input type="text" placeholder='Digite o nome do Autor' value={autor} onChange={ (e) => setAutor(e.target.value) } />
-        <button onClick={ () => handleAdd()}>Cadastrar</button>
-        <button onClick={ () => editPost()}>Editar</button>
-        <button onClick={ () => deletePost()}>Excluir</button>
-     </div>
-     
-     <div className="list-container">
-        <ul>
-        {
-          posts.map((post) =>{
-            return(
-                <li key={post.id}>
-                  <span>  {post.id } --  </span><button onClick={() => deletePostById(post.id) }> Excluir</button> <br />
-                  <span>  {post.titulo } -- </span> <br />
-                  <span>  {post.autor }</span> <br />
-                </li>
-            )
-          })
-        }
-        </ul>
-     </div>
-     
+    <h2>Cadastro de Novo Usuario</h2>
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder="Digite o Email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br />
+          
+          <label htmlFor="">Senha</label>
+          <input type="text" placeholder="Digite a Senha"  value={senha} onChange={(e) => setSenha(e.target.value)}/> <br />
+          
+          <button onClick={novoUsuario}>Cadastrar</button>
     </div>
+    
+    <div className='container'>
+        <div className="form-container">
+        <h2>Banco de Dados</h2>
+          <label htmlFor="">ID</label>
+          <input type="text" placeholder="Digite o Id" value={idPost} onChange={(e) => setIdPost(e.target.value)} />
+
+          <label htmlFor="">Titulo1</label>
+          <textarea placeholder="Digite o titulo" cols="10" rows="3" value={titulo} onChange={(e) => setTitulo(e.target.value)}></textarea>
+          <label htmlFor="">Autor</label>
+          <input type="text" placeholder='Digite o nome do Autor' value={autor} onChange={(e) => setAutor(e.target.value)} />
+          <button onClick={() => handleAdd()}>Cadastrar</button>
+          <button onClick={() => editPost()}>Editar</button>
+          <button onClick={() => deletePost()}>Excluir</button>
+        </div>
+
+        <div className="list-container">
+          <ul>
+            {posts.map((post) => {
+              return (
+                <li key={post.id}>
+                  <span>  {post.id} --  </span><button onClick={() => deletePostById(post.id)}> Excluir</button> <br />
+                  <span>  {post.titulo} -- </span> <br />
+                  <span>  {post.autor}</span> <br />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+      </div></>
   );
 }
 
